ad8659cc255c5638ab91b69afaac96d3ab8c5a48
sachchidanand2005 <sachcidanandrajbhar91@gmail.com>
2025-04-14 18:17:03 -0700

    completed

--- src/data/products.jsx
+++ src/data/products.jsx
@@ -1,28 +1,102 @@
 const products = [
     {
       id: 1,
-      name: "Classic T-Shirt",
+      name: "Classic Shirt",
       price: 19.99,
-      image: "https://via.placeholder.com/300x300?text=T-Shirt",
+      image: "/src/assets/shirt.webp",
     },
     {
       id: 2,
-      name: "Comfy Hoodie",
+      name: "fan",
       price: 39.99,
-      image: "https://via.placeholder.com/300x300?text=Hoodie",
+      image: "src/assets/fan.jpeg",
     },
     {
       id: 3,
-      name: "Stylish Cap",
+      name: "Desktop",
       price: 14.99,
-      image: "https://via.placeholder.com/300x300?text=Cap",
+      image: "/src/assets/desktop.jpeg",
     },
     {
       id: 4,
-      name: "Sneakers",
+      name: "book",
       price: 59.99,
-      image: "https://via.placeholder.com/300x300?text=Sneakers",
+      image: "/src/assets/book.jpeg",
     },
+    {
+      id: 5,
+      name: "phone",
+      price: 59.99,
+      image: "/src/assets/smartphone.jpeg",
+    },
+    {
+      id: 6,
+      name: "almond-oil",
+      price: 59.99,
+      image: "/src/assets/almond-oil.jpeg",
+    },
+    {
+      id: 7,
+      name: "cocnut-oil",
+      price: 59.99,
+      image: "/src/assets/coconut-oil.jpeg",
+    },
+    {
+      id: 8,
+      name: "cooler",
+      price: 59.99,
+      image: "/src/assets/cooler.jpeg",
+    },
+    {
+      id: 9,
+      name: "cpu",
+      price: 59.99,
+      image: "/src/assets/cpu.jpeg",
+    },
+    {
+      id: 10,
+      name: "laptop",
+      price: 59.99,
+      image: "/src/assets/download.jpeg",
+    },
+    {
+      id: 11,
+      name: "keyboard",
+      price: 59.99,
+      image: "/src/assets/keybord.jpeg",
+    },
+    {
+      id: 12,
+      name: "mouse",
+      price: 59.99,
+      image: "/src/assets/mouse.jpeg",
+    },
+    {
+      id: 13,
+      name: "perfume",
+      price: 59.99,
+      image: "/src/assets/perfume.jpeg",
+    },
+    {
+      id: 14,
+      name: "shoes",
+      price: 59.99,
+      image: "/src/assets/shoes.jpeg",
+    },
+    {
+      id: 15,
+      name: "phone",
+      price: 59.99,
+      image: "/src/assets/smartphone.jpeg",
+    },
+    {
+      id: 16,
+      name: "shirt",
+      price: 59.99,
+      image: "/src/assets/shirt.webp",
+    },
+
+
   ];
   
   export default products;
